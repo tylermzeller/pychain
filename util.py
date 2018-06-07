313cbbe388bd72248f5dd68613ef0988ef92346e
Tyler Zeller <tylermzeller@gmail.com>
2018-06-07 09:39:26 -0700

    Add missing util function

--- util.py
+++ util.py
@@ -1,6 +1,10 @@
 import struct
 
+# TODO: rename int64ToBinary
 def int64ToBytes(i):
     # TODO: error handling
     # >q means big endian, long long (int64)
     return struct.pack(">q", i)
+
+def intToBytes(i):
+    return bytes([i])
